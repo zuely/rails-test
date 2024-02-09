bd9808f46e53eae3dbb232de7320f7511d65fbb5
Eduardo Rossi <edurossi@gmail.com>
2024-02-08 18:11:48 -0700

    Remove some tests

--- spec/views/articles/edit.html.erb_spec.rb
+++ spec/views/articles/edit.html.erb_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'articles/edit', type: :view do
-  let(:article) do
-    Article.create!(
-      title: 'MyString',
-      author: 'MyString',
-      body: 'MyText'
-    )
-  end
+# RSpec.describe 'articles/edit', type: :view do
+#   let(:article) do
+#     Article.create!(
+#       title: 'MyString',
+#       author: 'MyString',
+#       body: 'MyText'
+#     )
+#   end
 
-  before(:each) do
-    assign(:article, article)
-  end
+#   before(:each) do
+#     assign(:article, article)
+#   end
 
-  it 'renders the edit article form' do
-    render
+#   it 'renders the edit article form' do
+#     render
 
-    assert_select 'form[action=?][method=?]', article_path(article), 'post' do
-      assert_select 'input[name=?]', 'article[title]'
+#     assert_select 'form[action=?][method=?]', article_path(article), 'post' do
+#       assert_select 'input[name=?]', 'article[title]'
 
-      assert_select 'input[name=?]', 'article[author]'
+#       assert_select 'input[name=?]', 'article[author]'
 
-      assert_select 'textarea[name=?]', 'article[body]'
-    end
-  end
-end
+#       assert_select 'textarea[name=?]', 'article[body]'
+#     end
+#   end
+# end
---
+++ spec/views/articles/view.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# RSpec.describe 'articles/view', type: :view do
+#   before(:each) do
+#     assign(:article, Article.create!(
+#                        title: 'Title',
+#                        author: 'Author',
+#                        body: 'MyText'
+#                      ))
+#   end
+
+#   it 'renders attributes in <p>' do
+#     render
+#     expect(rendered).to match(/Title/)
+#     expect(rendered).to match(/Author/)
+#     expect(rendered).to match(/MyText/)
+#   end
+# end
